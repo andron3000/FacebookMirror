505284e4bfa2c6ab1b48fc00aa9750d07df3fdac
andron3000 <chekhovych1995@gmail.com>
2017-05-08 17:22:20 +0300

    temp
    temp

--- src/main/java/com/chekhovych/model/Post.java
+++ src/main/java/com/chekhovych/model/Post.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 public class Post implements Serializable{
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "post_id")
     private Long id;
 
